b114aa42c19a0346851b0683e5b0d7a5afc0ac7f
Nikotiine <niko.tinne@gmail.com>
2023-01-28 19:00:50 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Une Voie Une Croix')
+    .setDescription('API nestJs , une voie une croix ')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT_DEV);
+}
+bootstrap();
