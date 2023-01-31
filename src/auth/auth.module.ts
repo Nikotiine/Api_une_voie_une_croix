25e7512034eb5215e0fc3576f8fff6bac5006d89
Nikotiine <niko.tinne@gmail.com>
2023-01-31 14:43:39 +0100

    config jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,17 +4,11 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { jwtConfig } from '../config/jwt.config';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports: [
-    UserModule,
-    PassportModule,
-    JwtModule.register({
-      secret: 'secretkeyachanger',
-      signOptions: { expiresIn: '60s' },
-    }),
-  ],
+  imports: [UserModule, PassportModule, JwtModule.registerAsync(jwtConfig)],
 })
 export class AuthModule {}
